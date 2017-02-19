42b80bf3826b0f345d5eec6763afbd63ca4cf89e
nishaya <nishaya@users.noreply.github.com>
2017-02-19 23:15:01 +0900

    on wheel event

--- src/components/controls/knob.jsx
+++ src/components/controls/knob.jsx
@@ -2,10 +2,31 @@ import React, { Component, PropType } from 'react'
 import style from './knob.scss'
 
 export default class Knob extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      rotation: -120,
+    }
+  }
+
+  rotate(amount, target) {
+    console.log(amount, target)
+    const rotation = this.state.rotation + amount
+    this.setState({ rotation })
+  }
+
   render() {
     return (<div className={style.container}>
       <div className={style.body}>
-        <div className={style.rotator}>
+        <div
+          onWheel={(e) => {
+            this.rotate(e.deltaY, e.target)
+          }}
+          style={{
+            transform: `rotate(${this.state.rotation}deg)`,
+          }}
+          className={style.rotator}
+        >
           <div className={style.cursor} />
         </div>
       </div>
