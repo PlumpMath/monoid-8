71772ae9d9653dfc8eda50dc2fcd5be5a918ca84
nishaya <nishaya@users.noreply.github.com>
2017-02-19 22:55:47 +0900

    rotator

--- src/components/controls/knob.jsx
+++ src/components/controls/knob.jsx
@@ -5,7 +5,9 @@ export default class Knob extends Component {
   render() {
     return (<div className={style.container}>
       <div className={style.body}>
-        <div className={style.cursor} />
+        <div className={style.rotator}>
+          <div className={style.cursor} />
+        </div>
       </div>
       knob
     </div>)
