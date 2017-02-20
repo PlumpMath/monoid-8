9417f22b00c523352e844ab7b7fff3076ac94080
nishaya <nishaya@users.noreply.github.com>
2017-02-20 15:39:51 +0900

    styling

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-// import { Card } from 'antd'
-import { Card, Col, Row } from 'antd'
+import { Card } from 'antd'
 import Knob from 'components/controls/knob'
 
 export default class IndexComponent extends Component {
@@ -15,19 +14,6 @@ export default class IndexComponent extends Component {
         <Knob label="param2" showValue />
         <Knob label="param3" showValue />
       </Card>
-      <div style={{ background: '#ECECEC', padding: '30px' }}>
-        <Row>
-          <Col span="8">
-            <Card title="Card title #1" bordered>Card content</Card>
-          </Col>
-          <Col span="8">
-            <Card title="Card title #2" bordered={false}>Card content</Card>
-          </Col>
-          <Col span="8">
-            <Card title="Card title" bordered={false}>Card content</Card>
-          </Col>
-        </Row>
-      </div>
     </div>)
   }
 }
